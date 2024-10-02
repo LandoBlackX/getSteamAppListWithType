a045cdd51efcd59715b31effe1677d9a4c99b153
F1NN1ER <51511170+F1NN1ER@users.noreply.github.com>
2024-10-02 22:18:01 +0800

    Update getAppList.py

--- Src/getAppList.py
+++ Src/getAppList.py
@@ -1,19 +1,22 @@
+import os
 import sqlite3
 import time
 
 import requests
 
-getAppList_URL = "https://api.steampowered.com/ISteamApps/GetAppList/v2/"
+db_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data', 'app_list.db')
 
+os.makedirs(os.path.dirname(db_path), exist_ok=True)
+
+getAppList_URL = "https://api.steampowered.com/ISteamApps/GetAppList/v2/"
 response = requests.get(getAppList_URL)
 
 if response.status_code == 200:
-    start_time = time.time()
     data = response.json()
     app_list = data['applist']['apps']
     app_ids = [(app['appid'],) for app in app_list]
 
-    conn = sqlite3.connect('app_list.db')
+    conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
 
     cursor.execute('''
@@ -26,6 +29,8 @@ if response.status_code == 200:
     conn.commit()
 
     new_count = 0
+    start_time = time.time()
+
     for app_id in app_ids:
         cursor.execute('''
             INSERT OR IGNORE INTO apps (appid) VALUES (?) ''', app_id)
@@ -36,6 +41,7 @@ if response.status_code == 200:
     conn.commit()
     end_time = time.time()
     conn.close()
+
     total_time = end_time - start_time
     print(f"新增 {new_count} 个 appid ,已成功写入数据库。")
     print(f"总耗费时间: {total_time:.6f} 秒")
