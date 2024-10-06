e97ecf7f3916aaa7aa01da7f064e7953693953df
F1NN1ER <51511170+F1NN1ER@users.noreply.github.com>
2024-10-06 15:22:54 +0800

    Add files via upload

---
+++ Src/action.mjs
@@ -0,0 +1,37 @@
+import { Octokit } from '@octokit/core';
+import https from 'https';
+
+// 设置全局环境变量
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+var counta = 0;
+const agent = new https.Agent({
+  rejectUnauthorized: false,
+});
+
+const octokit = new Octokit({
+  auth: '${{ secrets.Token }}',
+  request: {
+    agent: agent,
+  },
+});
+
+async function getWorkflows() {
+  try {
+    const response = await octokit.request(
+      'POST /repos/F1NN1ER/getSteamAppListWithType/actions/workflows/main.yml/dispatches',
+      {
+        ref: 'main',
+        headers: {
+          'X-GitHub-Api-Version': '2022-11-28',
+        },
+      }
+    );
+    counta++;
+    console.log(counta);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+getWorkflows();
+setInterval(getWorkflows, 70000);
