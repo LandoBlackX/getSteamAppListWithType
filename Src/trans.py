8f64641d10a065e8ce0e5344a6a23d2c51f0b168
F1NN1ER <51511170+F1NN1ER@users.noreply.github.com>
2024-10-07 17:19:26 +0800

    Update trans.py

--- Src/trans.py
+++ Src/trans.py
@@ -8,7 +8,7 @@ output_file = Path(os.path.dirname(os.path.dirname(__file__))) / 'data' / 'Listw
 with open(input_file, 'r', encoding='utf-8') as f:
     data = json.load(f)
 
-filtered_data = {k: v for k, v in data.items() if v not in ["game", "demo","video","movie","advertising","mod"]}
+filtered_data = {k: v for k, v in data.items() if v not in ["game", "demo","video","movie","advertising","mod","hardware"]}
 
 with open(output_file, 'w', encoding='utf-8') as f:
     json.dump(filtered_data, f, ensure_ascii=False, indent=4)
